e55b5c95a48449765ad8d7de44698cbe18f6bbca
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2022-03-11 23:12:24 +0100

    Add CORS support for localhost:4200

--- server/src/listeners/socketsManager.ts
+++ server/src/listeners/socketsManager.ts
@@ -4,7 +4,12 @@ import surveyHandler from './surveyHandler';
 let io: Io;
 
 export default function socketsManager(server) {
-    io = new Server(server);
+    io = new Server(server, {
+        cors: {
+          origin: "http://localhost:4200",
+          methods: ["GET", "POST"]
+        }
+      });
 
     io.on('connection', (socket) => {
         surveyHandler(io, socket);
